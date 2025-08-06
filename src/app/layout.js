058b6f059a353380a5a50a824c67d1b20ab1ff1e
Thomas <thomasnhenrique@gmail.com>
2025-08-06 20:48:16 -0300

    modificando layout para uma versao mais refinada

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,23 @@
 import { CarrinhoProvider } from "@/context/CarrinhoContext"
+import Navbar from "@/components/Navbar/Navbar"
+import './globals.css' // Importe seu arquivo CSS global
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
-      <body>
+    <html lang="pt-br" data-theme="light">
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>TecnozHub</title>
+      {/* Adicione outros metadados conforme necessário */}
+      <head>
+        {/* Metadados opcionais */}
+      </head>
+      <body className="min-h-screen bg-base-100"> {/* Adicione classes Tailwind */}
         <CarrinhoProvider>
-          {children}
+          <Navbar />
+          <main className="container mx-auto px-4 py-6"> {/* Container para o conteúdo */}
+            {children}
+          </main>
         </CarrinhoProvider>
       </body>
     </html>
