cb60f564d8939409ae2167c61802ffbfa9500e71
Thomas <thomasnhenrique@gmail.com>
2025-08-14 19:33:03 -0300

    PedidosHeader simples para descrever para que serve a page de pedidos

---
+++ src/components/pedidos/PedidosHeader/PedidosHeader.jsx
@@ -0,0 +1,8 @@
+export default function PedidosHeader() {
+  return (
+    <header className="mb-8">
+      <h1 className="text-3xl font-bold text-primary">Meus Pedidos</h1>
+      <p className="text-gray-500 mt-2">Acompanhe seu histórico de compras</p>
+    </header>
+  )
+}
