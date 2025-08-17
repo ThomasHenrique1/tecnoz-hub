228eeb2aa6a426520956618c3502d0a869524de0
Thomas <thomasnhenrique@gmail.com>
2025-08-17 19:51:05 -0300

    Alteracoes para funcionamento em browses diversificados e sem problema de interacao

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
@@ -9,6 +10,35 @@ const nextConfig = {
         pathname: '/storage/v1/object/public/**',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://ypxcrzudxqtntiksnydu.supabase.co/:path*',
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { 
+      ...config.resolve.fallback,
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "http": require.resolve("stream-http"),
+    };
+    return config;
   },
 };
 
