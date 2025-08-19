91a6dd447e57e1a0a27fe2e45a1b7b09771ace1e
Thomas <thomasnhenrique@gmail.com>
2025-08-18 22:07:38 -0300

    Melhorando pagina de login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,9 +2,7 @@ import LoginForm from '@/components/auth/LoginForm'
 
 export default function LoginPage() {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold text-center mb-6">Login</h1>
-      <p className="text-center mb-4">Faça login para acessar sua conta</p>
+    <div className="p-2">
       {/* Renderiza o formulário de login */}
       <LoginForm />
     </div>
