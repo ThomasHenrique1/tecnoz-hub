58c6080e0b2a648f3d72fc7d054c5bc4deb00dff
Thomas <thomasnhenrique@gmail.com>
2025-09-28 20:49:28 -0300

    Atualizando arquivo para importar como meta

--- next.config.mjs
+++ next.config.mjs
@@ -32,13 +32,13 @@ const nextConfig = {
     ];
   },
   webpack: (config) => {
-    config.resolve.fallback = { 
-      ...config.resolve.fallback,
-      "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "http": require.resolve("stream-http"),
-    };
-    return config;
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    crypto: import.meta.resolve("crypto-browserify"),
+    stream: import.meta.resolve("stream-browserify"),
+    http: import.meta.resolve("stream-http"),
+  };
+  return config;
   },
 };
 
