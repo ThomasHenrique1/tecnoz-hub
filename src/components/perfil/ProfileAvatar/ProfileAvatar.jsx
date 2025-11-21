a10b01e30621069f806554b70ef73570ee1e8b97
Thomas <thomasnhenrique@gmail.com>
2025-11-20 22:24:47 -0300

    Atualizando, utilizando novo componente global de button

--- src/components/perfil/ProfileAvatar/ProfileAvatar.jsx
+++ src/components/perfil/ProfileAvatar/ProfileAvatar.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Button from '@/components/ui/Button'
 import Image from 'next/image'
 import { useState } from 'react'
 
@@ -78,15 +79,14 @@ export function ProfileAvatar({
             <span className="text-sm text-center text-gray-500 truncate max-w-xs">
               {selectedFile.name}
             </span>
-            <button
+            <Button 
+              variant="outline"
+              iconRight={<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-left" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" /></svg>}
               onClick={onUpload}
               className="px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary-focus transition-colors flex items-center gap-2 w-full justify-center"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-              </svg>
               Enviar imagem
-            </button>
+            </Button>
           </div>
         )}
       </div>
